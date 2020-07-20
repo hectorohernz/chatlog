f73feeb403654a645fd34af9a2ca504910ecdbeb
Hector Hernandez <hectorohernz@gmail.com>
2020-07-20 14:25:14 -0400

    heroku

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const app = express();
+const app  = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const connectMongoDB = require('./config');
 
 app.use(cors());
 connectMongoDB();
-app.use(express.json({ extented: false, limit: '50mb' }));
+app.use(express.json({extented: false, limit: '50mb'}));
 
-// Setting Up Server
-app.get('/', (req, res) => {
-    res.send('Node Server Is Working !')
-})
 
 // Routes
 
@@ -19,10 +15,10 @@ app.use("/api/post", require("./routes/Post"));
 app.use("/api/feed", require("./routes/Feed"));
 
 
-app.use(express.static('client/build'));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {	 
+      res.sendFile(path.resolve(__dirname,  "./client/build", "index.html"));
+    });
 
 // Creating a server with node
 app.listen(PORT, () => {
